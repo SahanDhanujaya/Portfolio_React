c3b69c81eac4df85905df1bacd1ce78a9a07ab13
SahanDhanujaya <sahandhanujaya18@gmail.com>
2025-07-06 23:12:23 +0530

    name component is updated

--- src/components/nameComponent/Name.tsx
+++ src/components/nameComponent/Name.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import './name.css'
+import ShinyText from "../../animations/texts/shinyText/ShinyText";
+import "./name.css";
 
 function Name() {
   return (
     <div className="text-9xl font-bold font-londrina-outline text-center text-white min-w-full min-h-screen flex items-center justify-center absolute top-0 left-0 name">
-      <h1 className='text-[15vw] font-black text-transparent [text-stroke:2px_white]'>DHANUX</h1>
+      {/* <h1 className='text-[15vw] font-black text-transparent [text-stroke:2px_white]'>DHANUX</h1> */}
+
+      <ShinyText
+        text="DHANUX"
+        disabled={false}
+        speed={1}
+        className="custom-class"
+      />
     </div>
-  )
+  );
 }
 
-export default Name
+export default Name;
